33fc6669f56477ad46e02e1ac640d1e5563256f4
Trevor Mee <122403539+trevormee@users.noreply.github.com>
2025-06-12 16:52:12 +0000

    section 4 project complete

--- Section4/animals/src/App.js
+++ Section4/animals/src/App.js
@@ -1,18 +1,40 @@
-import React, { useState } from 'react';
+import './App.css';
+import { useState } from 'react';
+import AnimalShow from './AnimalShow';
 
+function getRandomAnimal() {
+    const animals = ['bird', 'cat', 'cow', 'dog', 'gator', 'horse'];
+
+    // Math.random() returns a decimal
+    // Math.floor() rounds down to an integer value
+    return animals[Math.floor(Math.random() * animals.length)]
+}
+console.log(getRandomAnimal());
 
 function App()  {
     
-    const [counter, setCount] = useState(0);
-
+    const [animals, setAnimals] = useState([]);
+    
     const handleClick = () => {
-        setCount(counter + 1);
+        setAnimals([...animals, getRandomAnimal()]);
     };
 
+    // When you call map, you take every element inside of the array,
+    // and 1 by 1, you pass every element into a transformation function.
+    // In this case, we are mapping each animal into the (animal, index)
+    // arrow function
+    // Ex: If you click the button once after restarting browser and the random animal
+    // is cat, cat is passed to the 'animal' paramter and it will have an index of 1
+    const renderedAnimals = animals.map((animal, index) => {
+        return <AnimalShow type={animal} key={index} />
+    });
+
     return ( 
-        <div>
+        <div className='app'>
             <button onClick={handleClick}>Add Animal</button>
-            <p>Times clicked {counter}</p>
+            <div className='animal-list'>
+                Animls to show: {renderedAnimals}
+            </div>
         </div>
     );
 }
