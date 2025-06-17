bac14423625f68c8b735c4a23f64fc60bfd1b9bd
Trevor Mee <trevor.mee@ds2.com>
2025-06-17 16:58:43 -0500

    Stopped after 167 of tutorial

--- Section10/comps/src/index.js
+++ Section10/comps/src/index.js
@@ -1,10 +1,13 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.js";
+// 1) Import the React and ReactDOM libs
+import React from 'react';;
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+// 2) Get a reference to the div with ID root
+const el = document.getElementById('root');
+
+// 3) Tell React to take control of that elemnt
+const root = ReactDOM.createRoot(el);
+
+// 4) Show the component on the screen
+root.render(<App />);
