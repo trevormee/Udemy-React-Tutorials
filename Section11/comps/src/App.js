776a5b244755595fe8f3ae91b5796667e220aee8
Trevor Mee <trevor.mee@ds2.com>
2025-06-18 12:39:33 -0500

    Copy contents of section 10 into section11 folder

---
+++ Section11/comps/src/App.js
@@ -0,0 +1,37 @@
+import { GoBell, GoCloudDownload, GoDatabase } from 'react-icons/go';
+import Button from './Button';
+
+function App() {
+
+    const handleClick = () => {
+        console.log('Clicked!');
+    };
+
+    return (
+        <div>
+            <div>
+                <Button success rounded outline className="mb-5" onClick={handleClick}>
+                    <GoBell />
+                    Click Me!
+                </Button>
+            </div>
+            <div>
+                <Button danger outline>
+                    <GoCloudDownload />
+                    Buy Now!
+                </Button>
+            </div>
+            <div>
+                <Button warning><GoDatabase /> See Deal!</Button>
+            </div>
+            <div>
+                <Button secondary>Hide Ads</Button>
+            </div>
+            <div>
+                <Button primary rounded>Something</Button>
+            </div>
+        </div>
+    );
+}
+
+export default App;
