29a447bc2f62f42c061e1aa4c504aa5deaebb0af
Trevor Mee <trevor.mee@ds2.com>
2025-06-18 17:03:56 -0500

    Finished for the day with section12

---
+++ Section12/comps/src/components/Dropdown.js
@@ -0,0 +1,47 @@
+
+import {useState} from 'react';
+import { GoChevronDown } from 'react-icons/go';
+import Panel from './Panel';
+
+function Dropdown({ options, value, onChange }) {
+
+    const [isOpen, setIsOpen] = useState(false);
+
+    const handleClick = () => {
+        // Close Dropdown
+        setIsOpen(!isOpen);
+    };
+
+    const handleOptionClick = (option) => {
+        // Close the dropdown
+        setIsOpen(false);
+        // What option did the user click on?
+        onChange(option);
+    };
+    
+    const renderedOptions = options.map((option) => {
+        return <div className='hover:bg-sky-100 rounded cursor-pointer p-1' onClick={() => handleOptionClick(option)} key={option.value}>
+            {option.label}
+        </div>
+    });
+
+    
+    return (
+        <div className='w-48 relative'>
+            <Panel className='flex justify-between items-center cursor-pointer'
+                 onClick={handleClick}>
+
+                {/* If selection is null, output 'Select...', else output the color*/}
+                {value?.label || 'Select...'}   
+                <GoChevronDown className='text-lg'/>
+            </Panel>
+            {isOpen && (
+                <Panel className='absolute top-full'>
+                    {renderedOptions}
+                </Panel>
+            )}
+        </div>
+    )
+}
+
+export default Dropdown;
